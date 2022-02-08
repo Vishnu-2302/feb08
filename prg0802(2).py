79d73a8adaaa7d48f7fab73987c6ce72f2606e9d
Vishnu-2302 <99253009+Vishnu-2302@users.noreply.github.com>
2022-02-08 20:23:28 +0530

    Add files via upload

---
+++ prg0802(2).py
@@ -0,0 +1,4 @@
+k=int(input('enter the number of kilometers\t:\t'))
+m=k*1000
+c=m*100
+print(k,'kilometers\n',m,'meters\n',c,'centimeters')      
---
+++ prg0802.py
@@ -0,0 +1,9 @@
+a=int(input('enter the a value\t:\t'))
+b=int(input('enter the b value\t:\t'))
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
